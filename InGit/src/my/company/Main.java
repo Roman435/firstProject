cbc389ed78f4bf00624f11dc6dbbd24c9967b00d
Roman Khlan <spacer435@gmail.com>
2018-12-29 12:05:01 +0200

    first commit

---
+++ InGit/src/my/company/Main.java
@@ -0,0 +1,11 @@
+package my.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello");
+	System.out.println("Hi");
+	String h = justClass.Get();
+	System.out.println(h);
+    }
+}
